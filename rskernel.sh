79a2046cbda0633d4a625af3290f40bb166d3f4c
UXH <24637370+uxh@users.noreply.github.com>
2018-04-16 17:24:05 +0800

    UPDATE Kernel Urls

--- rskernel.sh
+++ rskernel.sh
@@ -74,8 +74,8 @@ if check_system centos; then
     if check_centos_main_version 6; then
         echo -e "${yellow}System OS is CentOS6. Processing...${plain}"
         echo -e "-----------------------------------------"
-        rpm -ivh https://www.xiazaio.win/resource/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
-        rpm -ivh https://www.xiazaio.win/resource/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
+        rpm -ivh https://curls.fun/Resource/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
+        rpm -ivh https://curls.fun/Resource/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
         number=$(cat /boot/grub/grub.conf | awk '$1=="title" {print i++ " : " $NF}' | grep '2.6.32-504' | awk '{print $1}')
         sed -i "s/^default=.*/default=$number/g" /boot/grub/grub.conf
         echo -e "-----------------------------------------"
@@ -85,7 +85,7 @@ if check_system centos; then
     elif check_centos_main_version 7; then
         echo -e "${yellow}System OS is CentOS7. Processing...${plain}"
         echo -e "-----------------------------------------"
-        rpm -ivh https://www.xiazaio.win/resource/kernel-3.10.0-229.1.2.el7.x86_64.rpm --force
+        rpm -ivh https://curls.fun/Resource/kernel-3.10.0-229.1.2.el7.x86_64.rpm --force
         grub2-set-default `awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg | grep '(3.10.0-229.1.2.el7.x86_64) 7 (Core)' | awk '{print $1}'`
         echo -e "-----------------------------------------"
         echo -e "${green}Success! Your server will reboot in 3s...${plain}"
