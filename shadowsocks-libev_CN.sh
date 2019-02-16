17f7d90a5a424e9e201baeae10a985785aa64ad3
UXH <24637370+uxh@users.noreply.github.com>
2019-02-16 13:28:59 +0800

    Version 3.6
    update libsodium

--- shadowsocks-libev_CN.sh
+++ shadowsocks-libev_CN.sh
@@ -27,8 +27,8 @@ rc4-md5
 )
 
 #环境及程序版本控制
-libsodiumver="libsodium-1.0.16"
-libsodiumurl="https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz"
+libsodiumver="libsodium-1.0.17"
+libsodiumurl="https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz"
 mbedtlsver="mbedtls-2.13.0"
 mbedtlsurl="https://tls.mbed.org/download/mbedtls-2.13.0-gpl.tgz"
 shadowsocksver="shadowsocks-libev-3.2.0"
