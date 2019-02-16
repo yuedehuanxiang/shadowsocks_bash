7eb84392e3960db966e2548c5936b6e3b05154c6
UXH <24637370+uxh@users.noreply.github.com>
2019-02-16 13:32:02 +0800

    Version 3.7
    update mbedtls

--- shadowsocks-libev_CN.sh
+++ shadowsocks-libev_CN.sh
@@ -29,8 +29,8 @@ rc4-md5
 #环境及程序版本控制
 libsodiumver="libsodium-1.0.17"
 libsodiumurl="https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz"
-mbedtlsver="mbedtls-2.13.0"
-mbedtlsurl="https://tls.mbed.org/download/mbedtls-2.13.0-gpl.tgz"
+mbedtlsver="mbedtls-2.16.0"
+mbedtlsurl="https://tls.mbed.org/download/mbedtls-2.16.0-gpl.tgz"
 shadowsocksver="shadowsocks-libev-3.2.0"
 shadowsocksurl="https://github.com/shadowsocks/shadowsocks-libev/releases/download/v3.2.0/shadowsocks-libev-3.2.0.tar.gz"
 initscripturl="https://raw.githubusercontent.com/uxh/shadowsocks_bash/master/shadowsocks-libev"
